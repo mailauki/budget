41037b076d9d7406996daaba435b471ef57a8fa1
Julie Evans <juliemevans17@gmail.com>
2024-08-20 13:49:26 -0500

    add today card

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
-import CardContent from "@/components/card";
+"use client";
+import moment from "moment";
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+  Link,
+  Calendar,
+} from "@nextui-org/react";
+import { parseDate } from "@internationalized/date";
 
+const today = {
+  weekday: moment().format("dddd"),
+  year: moment().format("yyyy"),
+  month: moment().format("MMMM"),
+  date: moment().format("DD"),
+};
 
 export default function Home() {
   return (
@@ -48,7 +66,35 @@ export default function Home() {
       <div className="inline-block max-w-lg text-center justify-center">
         <h1>Hello World!</h1>
       </div>
-			<CardContent />
+      <Card className="max-w-[400px]">
+        <CardHeader className="flex gap-3">
+          <div className="flex flex-col">
+            <p className="text-[4rem]">{today.date}</p>
+            <p className="text-small text-default-500">
+              {today.month} {today.year}
+            </p>
+            <p className="text-small text-default-500">{today.weekday}</p>
+          </div>
+        </CardHeader>
+        <Divider />
+        <CardBody className="flex gap-3">
+          <Calendar
+            aria-label="Date (Uncontrolled)"
+            defaultValue={parseDate(moment().format("YYYY-MM-DD"))}
+          />
+          <p>Make beautiful websites regardless of your design experience.</p>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <Link
+            isExternal
+            showAnchorIcon
+            href="https://github.com/nextui-org/nextui"
+          >
+            Visit source code on GitHub.
+          </Link>
+        </CardFooter>
+      </Card>
     </section>
   );
 }

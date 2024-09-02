ee9f4f7c518c49a4286487bb45a139a1b8185870
Julie Evans <juliemevans17@gmail.com>
2024-09-02 12:07:00 -0500

    add goal edit

--- components/goals/card.tsx
+++ components/goals/card.tsx
@@ -1,13 +1,24 @@
 import {
+  Button,
   Card,
   CardFooter,
   CardHeader,
   Chip,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
   Progress,
+  useDisclosure,
 } from "@nextui-org/react";
 import { useNumberFormatter } from "@react-aria/i18n";
+import React from "react";
+
+import GoalForm from "./form";
 
 import { Goal } from "@/types";
+import { editGoal } from "@/db/actions";
 
 export default function GoalCard({ goal }: { goal: Goal }) {
   const formatter = useNumberFormatter({
@@ -15,48 +26,94 @@ export default function GoalCard({ goal }: { goal: Goal }) {
     currencyDisplay: "symbol",
     currencySign: "standard",
     style: "currency",
-    minimumFractionDigits: 2,
-    trailingZeroDisplay: "stripIfInteger",
-    roundingPriority: "auto",
+    minimumFractionDigits: 0,
+    // trailingZeroDisplay: "stripIfInteger",
+    // roundingPriority: "auto",
   });
   const progress = (100 * goal.current_amount) / goal.goal_amount;
 
+  const { isOpen, onOpen, onClose, onOpenChange } = useDisclosure();
+  const [openItem, setOpenItem] = React.useState<Goal>();
+
+  function handleOpen(item: Goal) {
+    setOpenItem(item);
+    onOpen();
+  }
+
   return (
-    <Card isFooterBlurred className="w-full h-[300px]">
-      <CardHeader className="absolute z-10 top-1 flex-col items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">Goal</p>
-        <h4 className="text-white/90 font-medium text-xl">{goal.name}</h4>
-      </CardHeader>
-      <div className="w-full h-full bg-gradient-to-tr from-pink-500 to-yellow-500" />
-      <CardFooter className="absolute bg-black/40 bottom-0 z-10">
-        <div className="flex flex-grow gap-2 items-center">
+    <>
+      <Card
+        isFooterBlurred
+        isPressable
+        className="w-full h-[300px]"
+        onPress={() => handleOpen(goal)}
+      >
+        <CardHeader className="absolute z-10 top-1 flex-col items-start">
+          <p className="text-tiny text-white/60 uppercase font-bold">Goal</p>
+          <h4 className="text-white/90 font-medium text-xl">{goal.name}</h4>
+        </CardHeader>
+        <div className="w-full h-full bg-gradient-to-tr from-pink-500 to-yellow-500" />
+        <CardFooter className="absolute bg-black/40 bottom-0 z-10">
+          <div className="flex flex-grow gap-2 items-center">
+            <Chip
+              className="text-md text-white bg-black/20"
+              color="default"
+              size="lg"
+              variant="flat"
+            >
+              {progress}%
+            </Chip>
+            <Progress
+              className="w-10/12"
+              classNames={{ indicator: "bg-foreground" }}
+              color="default"
+              value={progress}
+            />
+          </div>
           <Chip
-            className="text-md text-white bg-black/20"
+            className="text-tiny text-white bg-black/20"
             color="default"
-            radius="lg"
-            size="lg"
+            size="md"
             variant="flat"
           >
-            {progress}%
+            {formatter.format(goal.current_amount)} /{" "}
+            {formatter.format(goal.goal_amount)}
           </Chip>
-          <Progress
-            className="w-10/12"
-            classNames={{ indicator: "bg-foreground" }}
-            color="default"
-            value={progress}
-          />
-        </div>
-        <Chip
-          className="text-tiny text-white bg-black/20"
-          color="default"
-          radius="lg"
-          size="lg"
-          variant="flat"
-        >
-          {formatter.format(goal.current_amount)} /{" "}
-          {formatter.format(goal.goal_amount)}
-        </Chip>
-      </CardFooter>
-    </Card>
+        </CardFooter>
+      </Card>
+      <Modal
+        isOpen={isOpen}
+        radius="sm"
+        onClose={onClose}
+        onOpenChange={onOpenChange}
+      >
+        <ModalContent action={editGoal} as="form">
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Edit goal
+              </ModalHeader>
+              <ModalBody>
+                <GoalForm item={openItem} />
+              </ModalBody>
+              <ModalFooter>
+                <Button radius="sm" variant="bordered" onPress={onClose}>
+                  Close
+                </Button>
+                <Button
+                  className="bg-foreground text-background"
+                  color="primary"
+                  radius="sm"
+                  type="submit"
+                  onPress={onClose}
+                >
+                  Update
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
